23dc8023822e9c3b3d934b6be310638d6e24bfcb
blackki9 <blackki9@gmail.com>
2015-06-06 17:24:07 +0300

    Initial Commit

---
+++ OneHandReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OneHandReader
+//
+//  Created by black9 on 06/06/15.
+//  Copyright (c) 2015 black9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OneHandReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OneHandReader
+//
+//  Created by black9 on 06/06/15.
+//  Copyright (c) 2015 black9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
